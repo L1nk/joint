88971e657d8e0067409cdaaa954e73667d6738af
Paulo Panaligan <panaliga@gmail.com>
2013-05-20 15:11:53 -0700

    master [modularized server]

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var http = require("http");
 
-function onRequest(request, response) {
-    console.log('Request recieved.');
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World, from jpcp");
-    response.end();
+function start() {
+
+    function onRequest(request, response) {
+        console.log('Request recieved.');
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World, from jpcp");
+        response.end();
+    }   
+
+    http.createServer(onRequest).listen(8888);
+    console.log('Server started.');
+
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server started.');
+exports.start = start;
