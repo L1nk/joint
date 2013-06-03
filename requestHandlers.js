f360e6e07690dbc40e1a2dfac53518c7fcc9454d
Paulo Panaligan <panaliga@gmail.com>
2013-06-03 15:56:45 -0700

    master [image uploading complete]

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,16 +1,17 @@
-var querystring = require("querystring");
-	fs = require("fs")
+var querystring = require("querystring"),
+	fs = require("fs"),
+	formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
 	console.log("Request handler 'start' was called.");
 	var body = '<html>' +
 		'<head>' +
 		'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
 		'</head>' +
 		'<body>' +
-		'<form action="/upload" method="post">' +
-		'<textarea name="text" rows="20" cols="60"></textarea>' + 
-		'<input type="submit" value="Submit text" />'
+		'<form action="/upload" enctype="multipart/form-data" method="post">' +
+		'<input type="file" name="upload" multiple="multiple">' + 
+		'<input type="submit" value="Upload File" />'
 		'</form>' +
 		'</body>' +
 		'</html>';
@@ -20,16 +21,28 @@ function start(response, postData) {
 		response.end;
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent the text: " + querystring.parse(postData).text);
-	response.end();
+	var form = new formidable.IncomingForm();
+	console.log("upload: about to parse");
+	form.parse(request, function(error, fields, files) {
+		console.log("upload: parsing done");
+		fs.rename(files.upload.path, "tmp/test.png", function(error) {
+			if (error) {
+				fs.unlink("tmp/test.png");
+				fs.rename(files.upload.path, "tmp/test.png")
+			}
+		});
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("Received image: <br/>");
+		response.write("<img src='/show' />");
+		response.end();
+	});
 }
 
-function show(response, postData) {
+function show(response) {
 	console.log("Request handler 'show' was called.");
-	fs.readFile("", "binary", function(error, file) {
+	fs.readFile("tmp/test.png", "binary", function(error, file) {
 		if (error) {
 			response.writeHead(500, {"Content-Type": "text/plain"});
 			response.write(error + "\n");
@@ -44,4 +57,4 @@ function show(response, postData) {
 
 exports.start = start;
 exports.upload = upload;
-exports.shot = show;
+exports.show = show;
--- server.js
+++ server.js
@@ -3,17 +3,10 @@ var url = require("url");
 
 function start(route, handle) {
 
-    function onRequest(request, response) {
-        var postData = "";
+    function onRequest(request, response) {       
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " recieved.");
-        request.setEncoding("utf8");
-        request.addListener("data", function(postDataChunk) {
-            console.log("Recieved POST data chunk '" + postDataChunk + "'.");
-        });
-        request.addListener("end", function() {
-            route(handle, pathname, response, postData);
-        });
+        route(handle, pathname, response, request);
     }   
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
